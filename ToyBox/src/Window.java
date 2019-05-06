8fc35e981cb14e23eeb3d99856a808079b4a2cc4
Derek <35339062+Arnie-D@users.noreply.github.com>
2019-05-06 17:33:54 -0400

    Add files via upload

--- ToyBox/src/Window.java
+++ ToyBox/src/Window.java
@@ -18,6 +18,9 @@ public class Window implements ActionListener, ItemListener
 {
    protected JFrame frame;
    
+   public static final Color LUMA_BLUE = new Color(173, 255, 255);
+   public int borderSize;
+   
    Runner runner; // extends Canvas, and you can add a canvas to a Content Pane
    JPanel runnerPanel;
    Setting setting;
@@ -28,6 +31,9 @@ public class Window implements ActionListener, ItemListener
    public String title;
    public int width, height;
    
+   public JPanel canvasHolder;
+   
+   public Color bottomPanelBG;
    
    public JPanel bottomBar;
    public int canvasHeight, bottomBarWidth, bottomBarHeight;
@@ -38,20 +44,21 @@ public class Window implements ActionListener, ItemListener
 
   
    public Window(int width, int height, String title, Runner runner)
-	{  
-      this.title = title;
+   {  
+      borderSize = 20;
+	   
+	  this.title = title;
       this.runner = runner;
       
       this.width = width;
       this.height = height;
       
+      bottomPanelBG = LUMA_BLUE;
+      
       bottomBarWidth = this.width;
       bottomBarHeight = 100;
       
       canvasHeight = height;
-      this.height = canvasHeight + bottomBarHeight;
-      
-      bottomBar = createBottomBar();
       
       createGUI();
       
@@ -59,77 +66,118 @@ public class Window implements ActionListener, ItemListener
       runner.start();
    }
    
+   public JPanel createCanvasHolder()
+   {
+	   JPanel canvasHolder = new JPanel(new BorderLayout());
+	   
+	   canvasHolder.setBounds(0,0, width, canvasHeight);
+	   
+	   canvasHolder.add(runner, BorderLayout.CENTER);
+	   
+	   return canvasHolder;
+	   
+   }
+   
    
    public JPanel createBottomBar()
    {
-      JPanel bottomPanel = new JPanel();
-      //bottomPanel.setLayoutManager();
+       JPanel bottomPanel = new JPanel(new FlowLayout());
       
-      JButton button1 = new JButton("Button 1");
+       JButton button1 = new JButton("Button 1");
       
-      bottomPanel.setBounds(10, 10, bottomBarWidth, bottomBarHeight);
+       bottomPanel.setBounds(0, 0, bottomBarWidth, bottomBarHeight);
+       bottomPanel.setBackground(bottomPanelBG);
       
-      button1.setActionCommand("button1");
+       button1.setActionCommand("button1");
+       button1.addActionListener(new ActionListener() {
+    	   public void actionPerformed(ActionEvent e){  
+               System.out.println(button1.getActionCommand() + " has been pressed"); 
+    	   }
+       });
       
-      bottomPanel.add(button1);
+       bottomPanel.add(button1);
       
-      return bottomPanel;
+       return bottomPanel;
    }
    
    public JMenuBar createJMenuBar() 
    {
-		JMenuBar mainMenuBar;
-      JMenu menu1;
-      JMenuItem plainTextMenuItem;
+	   JMenuBar mainMenuBar;
+       JMenu menu1;
+       JMenuItem toMenu;
+       JMenuItem close;
       
-      mainMenuBar = new JMenuBar();
-      menu1 = new JMenu("Menu");
-      mainMenuBar.add(menu1);
+       mainMenuBar = new JMenuBar();
+       menu1 = new JMenu("Menu");
+       mainMenuBar.add(menu1);
       
-      plainTextMenuItem = new JMenuItem("To Main Menu");
-      plainTextMenuItem.addActionListener(this);
-		menu1.add(plainTextMenuItem);
+       close = new JMenuItem("Close Window");
+       close.addActionListener(new ActionListener() {
+    	   public void actionPerformed(ActionEvent e){  
+               System.out.println("Close Window has been pressed"); 
+    	   }
+       });
+       
+       toMenu = new JMenuItem("To Main Menu");
+       toMenu.addActionListener(new ActionListener() {
+    	   public void actionPerformed(ActionEvent e){  
+               System.out.println("To Menu has been pressed"); 
+    	   }
+       });
+	   
+       menu1.add(toMenu);
+       menu1.add(close);
       
-      return mainMenuBar;
+       return mainMenuBar;
    }
 
    
    private void createGUI() 
    {
-		JFrame.setDefaultLookAndFeelDecorated(true);
-		// Create and set up the window.
-		JFrame frame = new JFrame(title);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-      Window app = this;
-		
-      frame.setJMenuBar(app.createJMenuBar());
-		frame.setContentPane(app.createContentPane());
+	   JFrame.setDefaultLookAndFeelDecorated(true);
+	      
+	   canvasHolder = createCanvasHolder();
+	   bottomBar = createBottomBar();
+	   
+	   // Create and set up the window.
+	   JFrame frame = new JFrame(title);
+	   frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	   frame.setLayout(new BorderLayout());
+	   
+       Window app = this;
       
-      frame.setPreferredSize(new Dimension(width, height));
-      frame.setMaximumSize(new Dimension(width, height));
-		frame.setMinimumSize(new Dimension(width, height));
-		frame.setResizable(false);
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
+       frame.setPreferredSize(new Dimension(width, height));
+       frame.setMaximumSize(new Dimension(width, height));
+	   frame.setMinimumSize(new Dimension(width, height));
+	   frame.setResizable(false);
+	   frame.setLocationRelativeTo(null);
+	   
+	   frame.setJMenuBar(app.createJMenuBar());
+	   frame.getContentPane().add(app.createContentPane());
+	   
+	   frame.setVisible(true);
    }
       
    public Container createContentPane() 
    {
-		// Create the content-pane-to-be.
-		JPanel jplContentPane = new JPanel(new BorderLayout());
-		jplContentPane.setLayout(new BorderLayout());
-		jplContentPane.setOpaque(true);
+	   // Create the content-pane-to-be.
+	   JPanel jplContentPane = new JPanel(new BorderLayout(10,10));
+	   jplContentPane.setOpaque(true);
+       jplContentPane.setPreferredSize(new Dimension(width + borderSize, height + borderSize));
+       jplContentPane.setMaximumSize(new Dimension(width + borderSize, height + borderSize ));
+       jplContentPane.setMinimumSize(new Dimension(width + borderSize, height + borderSize));
+	   jplContentPane.setBackground(Color.DARK_GRAY);
+	   
+	   jplContentPane.add(canvasHolder, BorderLayout.CENTER);
+	   jplContentPane.add(bottomBar, BorderLayout.PAGE_END);
       
-      jplContentPane.add(runner);
-      jplContentPane.add(bottomBar, BorderLayout.PAGE_END);
-      
-		return jplContentPane;
+	   return jplContentPane;
   }
   
   public void actionPerformed(ActionEvent e) 
   {
-		JMenuItem menuSource;
+	  /*
+	  JMenuItem menuSource;
       JButton buttonSource;
       
       JMenuItem menuItemReference = new JMenuItem();
@@ -149,7 +197,7 @@ public class Window implements ActionListener, ItemListener
          String buttonString = buttonSource.getActionCommand();
          System.out.println(buttonString + " has been pressed");
       }
-      
+      */
 	}
    
    public void itemStateChanged(ItemEvent e) {
